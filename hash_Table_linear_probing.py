6ca290d62308a2f3bc2a6801dfa358a8d4f137e3
kapiljasmera <jasmera.kapil@gmail.com>
2022-11-26 20:41:59 +0530

    Add files via upload

---
+++ hash_Table_linear_probing.py
@@ -0,0 +1,73 @@
+class Hash:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        return hash % self.MAX
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        if self.arr[h] is None:
+            return
+        prob_range = self.get_prob_range(h)
+        for prob_index in prob_range:
+            element = self.arr[prob_index]
+            if element is None:
+                return
+            if element[0] == key:
+                return element[1]
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        if self.arr[h] is None:
+            self.arr[h] = (key, val)
+        else:
+            new_h = self.find_slot(key, h)
+            self.arr[new_h] = (key, val)
+
+    def get_prob_range(self, index):
+        return [*range(index, len(self.arr))] + [*range(0, index)]
+
+    def find_slot(self, key, index):
+        prob_range = self.get_prob_range(index)
+        for prob_index in prob_range:
+            if self.arr[prob_index] is None:
+                return prob_index
+            if self.arr[prob_index][0] == key:
+                return prob_index
+        raise Exception("Hashmap full")
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        prob_range = self.get_prob_range(h)
+        for prob_index in prob_range:
+            if self.arr[prob_index] is None:
+                return
+            if self.arr[prob_index][0] == key:
+                self.arr[prob_index] = None
+                break
+
+
+if __name__ == "__main__":
+    k = Hash()
+    k["march 6"] = 30
+    k["march 17"] = 50
+    k["march 19"] = 70
+    k["march 20"] = 70
+    k["march 12"] = 70
+    k["march 1"] = 70
+    k["march 2"] = 70
+    k["march 3"] = 70
+    k["march 4"] = 70
+    k["march 5"] = 0
+    del k["march 6"]
+    print(k.arr)
+    print(k["march 6"])
+
+    del k["march 6"]
+
+    print(k.arr)
